9d5428e0cc254bf604d444eff407e5ad0ae0653e
carlobrok <carlo.brokering@outlook.de>
2019-06-14 14:59:35 +0200

    Not using wiringPi for I2C communication anymore

--- I2C_communication_recieve.cpp
+++ I2C_communication_recieve.cpp
@@ -2,13 +2,35 @@
 #include <thread>
 //#include <wiringPiI2C.h>
 #include <linux/i2c-dev.h>
-
+#include <sys/ioctl.h>
+#include <fcntl.h>
 
 using namespace std;
 
+int arduinoI2Copen(int devId) {
+//	int rev = piGpioLayout();
+	const char *device ;
+	int fd;
+
+//	if (rev == 1)
+//		device = "/dev/i2c-0" ;
+//	else
+	device = "/dev/i2c-1" ;
+
+	if((fd = open(device, O_RDWR)) < 0) {
+		return -1;
+	}
+
+	if (ioctl (fd, I2C_SLAVE, devId) < 0)
+		return -1;
+
+	return fd;
+}
+
+
 int main() {
 
-	int arduino_fd = wiringPiI2CSetup(0x08);
+	int arduino_fd = arduinoI2Copen(8);
 
 	if(arduino_fd == -1) {
 		cout << "Error in opening i2c bus to arduino" << endl;
@@ -17,7 +39,7 @@ int main() {
 
 	while(1) {
 
-		cout << wiringPiI2CRead(arduino_fd) << endl;
+		//cout << wiringPiI2CRead(arduino_fd) << endl;
 
 		std::this_thread::sleep_for(2s);
 
