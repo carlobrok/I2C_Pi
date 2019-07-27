17de252a12c6efa8b396b966d39d7b99299a698a
carlobrok <carlo.brokering@outlook.de>
2019-07-27 22:44:03 +0200

    Rewrite example

--- I2C_communication_recieve.cpp
+++ I2C_communication_recieve.cpp
@@ -2,27 +2,37 @@
 #include <bitset>
 #include <stdlib.h>
 #include <thread>
+#include <chrono>
 #include "KamelI2C.h"
 
 
 using namespace std;
 
-
-
 int main() {
 
-	int arduino_fd = kamelI2Copen(0x08);
+	int sensor_fd = kamelI2Copen(0x09);
 
-	if(arduino_fd == -1) {
-		cout << "Error in opening i2c bus to arduino" << endl;
+	if(sensor_fd == -1) {
+		cout << "Error in opening i2c bus to sensor arduino" << endl;
 		return -1;
 	}
 
+	uint8_t in_data[2];
+	bool dig_sensors[8];
+	uint16_t analog_ir[1];
+
 	while(1) {
 
-		int data[3];
+		getSensorData(sensor_fd, dig_sensors, analog_ir);
 
-		std::this_thread::sleep_for(2s);
+		cout << "Digital Sensors: " << endl;
+		for(int i = 0; i < 8; i++) {
+			cout << i << ": " << dig_sensors[i] << endl;
+		}
+		cout << endl;
+		cout << "Analog Sensor: " << analog_ir[0] << endl;
+
+		this_thread::sleep_for(chrono::milliseconds(500));
 
 	}
 }
